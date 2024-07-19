3f314d06f72deb3c38e5ad08e863dd4166a683b8
rishinagendramarre360 <rishinagendramarre360@gmail.com>
2024-07-19 14:41:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+
+let x = document.getElementById("sb")
+let count = 0
+
+function inc1() {
+    count += 1 
+    x.innerText = count
+}
+
+function inc2() {
+    count += 2 
+    x.innerText = count
+}
+
+function inc3() {
+    count += 3 
+    x.innerText = count
+}
+
+
+
+let y = document.getElementById("sb2")
+let count2 = 0
+
+function inc11() {
+    count2 += 1 
+    y.innerText = count2
+}
+
+function inc12() {
+    count2 +=2
+    y.innerText = count2
+}
+
+function inc13() {
+    count2 += 3 
+    y.innerText = count2
+}
+
+
